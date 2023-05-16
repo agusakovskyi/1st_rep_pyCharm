80cc85557dd822fc87b2e697574e586ae6fe5b8a
Artem <gusakovskyiartem@ukr.net>
2023-05-16 23:06:33 +0300

    Search_Item_on_site_Rozetka

--- utils/web_driver.py
+++ utils/web_driver.py
@@ -3,6 +3,8 @@ from selenium.webdriver import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
+import re
+
 
 class CustomWebDriver:
     def __init__(self):
@@ -14,6 +16,7 @@ class CustomWebDriver:
         self.search_input = (By.XPATH, "//input[@name='search']")
         self.pistol_1st = "//div[@class='product-card__image-wrapper']//img[contains(@alt, 'пістолет')][1]"
         self.language_selector = (By.XPATH, "//span[@class='button__text' and contains (text(),'UA')]")
+        self.search_title = (By.XPATH, "//h1[@class='catalog-heading ng-star-inserted']")
 
 
     def open_url(self):
@@ -35,11 +38,10 @@ class CustomWebDriver:
     def search_item(self, search_text):
         self.input_text(self.search_input, search_text)
         self.click_web_element(self.search_icon)
-
-
-
-
+        current_title = self.wait_web_element(self.search_title).text
+        current_title = current_title.replace('«', '').replace('»', '')
+        assert search_text == current_title, f"Actual title: {current_title} doesn't match with expected: {search_text}"
 
 custom_driver = CustomWebDriver()
 custom_driver.open_url()
-custom_driver.search_item("Colt")
+custom_driver.search_item("Принтер")
