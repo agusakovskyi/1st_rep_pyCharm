8783568f381c8a380229814eafd23281b94191b4
Artem <gusakovskyiartem@ukr.net>
2023-06-29 01:18:25 +0300

    added tests for ui Rozetka

--- tests/tests_ui_rozetka.py
+++ tests/tests_ui_rozetka.py
@@ -7,7 +7,23 @@ class TestsRozetka(unittest.TestCase):
         cls.rozetka_utils = RozetkaUtils()
         cls.rozetka_utils.open_rozetka_page()
 
+    def test_search_random_item(self):
+        self.rozetka_utils.search_random_item('asdfghjklo;p')
+
+    def test_field_clear_button(self):
+        self.rozetka_utils.validate_field_clear_button('some text')
+
     def test_search_item_via_main_search(self):
         self.rozetka_utils.search_item('Принтер')
         self.rozetka_utils.select_item_from_grid('Принтер')
         self.rozetka_utils.validate_selected_product_in_cart()
+
+    def test_search_camelcase_item_via_main_search(self):
+        self.rozetka_utils.search_item('пРиНтЕр')
+        self.rozetka_utils.select_item_from_grid('пРиНтЕр')
+        self.rozetka_utils.validate_selected_product_in_cart()
+
+
+
+
+
--- utils/rozetka_utils.py
+++ utils/rozetka_utils.py
@@ -6,8 +6,9 @@ class RozetkaUtils:
         self.url = "https://rozetka.com.ua/ua/"
         self.custom_driver = CustomWebDriver()
 
-        self.search_icon = (By.XPATH, "//button[contains(@class, 'submit ng-star-inserted')]")
+        self.search_icon = (By.XPATH, "//button[contains(@class, 'search-form__submit')]")
         self.search_input = (By.XPATH, "//input[@name='search']")
+        self.entered_text = "//li[contains(@class, 'search-suggest__item')]//span[contains(@class, 'search-suggest__item-request')]"
         self.search_title = (By.XPATH, "//h1[@class='catalog-heading ng-star-inserted']")
         self.search_item_grid = (By.XPATH, "//li[contains(@class, 'catalog-grid')]//a[contains(@title, '%s')]")
         self.main_item_title = (By.XPATH, "//h1[@class='product__title']")
@@ -15,6 +16,8 @@ class RozetkaUtils:
         self.buy_button = (By.XPATH, "//rz-product-buy-btn[contains(@class, 'product-button__buy')]//button")
         self.current_prod_price = (By.XPATH, "//div[contains(@class, 'cart-receipt__sum-price')]//span")
         self.current_item_title = (By.XPATH, "//div[contains(@class, 'cart-product__main')]//a")
+        self.field_clear_button = (By.XPATH, "//div[contains(@class, 'search-form__inner')]//button[contains(@class, 'search-form__clear')]")
+        self.result_random_search = (By.XPATH, "//div[contains(@class, 'catalog-empty')]//span[contains(@class, 'ng-star-inserted')]")
 
 
     def open_rozetka_page(self):
@@ -44,6 +47,40 @@ class RozetkaUtils:
         current_title = self.custom_driver.wait_web_element(self.current_item_title).text
         assert price_on_page == current_price, f"Actual price: {current_price} doesn't match with expected: {price_on_page}"
         assert self.title == current_title, f"Actual item: {current_title} doesn't match with expected: {self.title}"
+
+    def validate_field_clear_button(self, search_text):
+        self.custom_driver.input_text(self.search_input, search_text)
+        search_element = self.custom_driver.wait_web_element(self.search_input)
+        element_text = search_element.get_attribute('value')
+        self.custom_driver.click_web_element(self.field_clear_button)
+        current_field = self.custom_driver.wait_web_element(self.search_input)
+        current_text = current_field.get_attribute('value')
+        assert current_text == ''
+
+    def search_random_item(self, random_text):
+        self.custom_driver.input_text(self.search_input, random_text)
+        self.custom_driver.click_web_element(self.search_icon)
+        actual_result = self.custom_driver.wait_web_element(self.result_random_search).text
+        expected_result = 'За заданими параметрами не знайдено жодної моделі'
+        assert actual_result == expected_result, f"Actual result: {actual_result} doesn't match with expected {expected_result}"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
